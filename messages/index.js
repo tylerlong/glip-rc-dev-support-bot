5ba8b88bc7e323d2c6cdc4ab20039b06ae563bc6
Tyler Long <tyler4long@gmail.com>
2018-01-19 09:49:28 +0800

    Integrate Lex

--- messages/index.js
+++ messages/index.js
@@ -1,10 +1,14 @@
 import RingCentral from 'ringcentral-js-concise'
+import Lex from 'aws-sdk/clients/lexruntime'
 
 const token = JSON.parse(process.env.GLIP_API_TOKEN)
 const rc = new RingCentral('', '', process.env.GLIP_API_SERVER)
 rc.token(token)
 
+const lex = new Lex({ region: 'us-east-1' })
+
 export const handleMessage = (event, context, callback) => {
+  console.log(JSON.stringify(event, null, 2))
   if (event.headers['Verification-Token'] !== process.env.GLIP_VERIFICATION_TOKEN) {
     callback(null, { statusCode: 404 })
     return
@@ -15,10 +19,21 @@ export const handleMessage = (event, context, callback) => {
   }
   const message = JSON.parse(event.body).body
   if (message && message.creatorId !== token.owner_id) {
-    rc.post('/restapi/v1.0/glip/posts', {
-      groupId: message.groupId,
-      text: JSON.stringify(message, null, 2),
-      attachments: undefined
+    lex.postText({
+      botAlias: 'order_rose', /* required */
+      botName: 'OrderFlowers', /* required */
+      inputText: message.text, /* required */
+      userId: message.creatorId /* required */
+    }, (error, data) => {
+      console.log(error, data)
+      if (error != null) {
+        return
+      }
+      rc.post('/restapi/v1.0/glip/posts', {
+        groupId: message.groupId,
+        text: data.message,
+        attachments: undefined
+      })
     })
   }
 }
